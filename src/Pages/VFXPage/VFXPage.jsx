7f3c2b2e713ce8fb5a9cf5ada740ba283792893f
ilaria-longo <157503715+ilaria-longo@users.noreply.github.com>
2024-05-30 12:14:31 -0400

    vimeo demo reel has been embedded in the VFX page, needs styling and adjusting. Overall the functionality is there

--- src/Pages/VFXPage/VFXPage.jsx
+++ src/Pages/VFXPage/VFXPage.jsx
@@ -1,31 +1,59 @@
 import Player from "@vimeo/player";
 import React, { useEffect, useRef } from "react";
+import "./VFXPage.scss";
 
 const VFXPage = () => {
   const videoId = "944512360";
   //https://vimeo.com/manage/videos/944512360
   const playerRef = useRef(null);
-  useEffect(() => {
-    const player = new Player(playerRef.current, {
-      id: videoId,
-      width: "100%",
-    });
-    return () => {
-      player.unload(); // clean up the player instance
-    };
-  }, [videoId]);
+  // useEffect(() => {
+  //   const player = new Player(playerRef.current, {
+  //     id: videoId,
+  //     width: "100%",
+  //   });
+  //   return () => {
+  //     player.unload(); // clean up the player instance
+  //   };
+  // }, [videoId]);
 
   return (
     <div>
-      <h1>Demo Reel</h1>
+      <div className="title">
+        <h1>Ilaria's Demo Reel</h1>
+      </div>
       <div
+
+      // style={{ padding: "56.25% 0 0 0", position: "relative" }}
+      >
+        <iframe
+          className="video"
+          src="https://player.vimeo.com/video/944512360?badge=0&amp;autopause=0&amp;player_id=0&amp;app_id=58479"
+          frameborder="0"
+          allow="autoplay; fullscreen; picture-in-picture; clipboard-write"
+          style={{
+            position: "absolute",
+            // top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+          }}
+          title="Ilaria Longo - Compositing Showreel"
+        ></iframe>
+      </div>
+      {/* <script src={{"https://player.vimeo.com/api/player.js"}}></script> */}
+      {/* <div>
+        <h1>Demo Reel</h1>
+      </div>
+      <div
+        className="video"
         ref={playerRef}
         style={{
-          padding: "56.25% 0 0 0",
           position: "relative",
+          width: "700px",
+          height: "auto",
         }}
       />
-      <script src="https://player.vimeo.com/api/player.js"></script>
+      <script src="https://player.vimeo.com/api/player.js"></script> */}
     </div>
   );
 };
